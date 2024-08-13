089af49e65d5bc5bdf1a6aa76cd3bd997ede5963
Muhammad Abdullah <139040726+Abdullah6346@users.noreply.github.com.>
2024-08-13 14:10:52 +0500

    feat: Markdown Component Created
    I have created  a component for markdown

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,25 @@
 import "./App.css";
 import Markdown from "react-markdown";
 import { useState } from "react";
+import markdown from "./Components/markdown";
+
 function App() {
-  const [editor, seteditor] = useState("");
+  const [editor, seteditor] = useState(markdown);
+
   const handlechange = (e: Event) => {
     const target = e.target as HTMLInputElement;
     seteditor(target.value);
   };
-  const markdown = `# Hi, *Pluto*!`;
+
   return (
     <>
       <div className="Wrapper_tex_ar  ">
-        <textarea name="editor" id="editor" onChange={handlechange}></textarea>
+        <textarea
+          name="editor"
+          id="editor"
+          defaultValue={editor}
+          onChange={handlechange}
+        ></textarea>
       </div>
       <div id="Preview">
         <Markdown>{markdown}</Markdown>
