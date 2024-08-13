c0faa034989c7e34c433d3a8525038c88ab0a12e
Muhammad Abdullah <139040726+Abdullah6346@users.noreply.github.com.>
2024-08-13 11:03:52 +0500

    feat: Added  The Editor and Previewer

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,20 @@
-
-import './App.css'
-
+import "./App.css";
+import { useState } from "react";
 function App() {
-
+  const [editor, seteditor] = useState("");
+  const handlechange = (e: Event) => {
+    const target = e.target as HTMLInputElement;
+    seteditor(target.value);
+  };
   return (
     <>
-    <div className='Wrapper_tex_ar   '>
-  <textarea name="" id="editor"></textarea>
-
-    </div>
-    <div id='Preview'></div>
+      <div onChange={handlechange} className="Wrapper_tex_ar  ">
+        <textarea name="editor" id="editor"></textarea>
+      </div>
+      <div id="Preview">{editor}</div>
     </>
-  )
+  );
 }
 // khg?
 
-export default App
+export default App;
